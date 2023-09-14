168963130270fa27b2cd0bba27184689218a9e32
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-09-14 10:44:25 -0300

    feat: add binary tree

---
+++ internal/tree/binary_tree.go
@@ -0,0 +1,49 @@
+package tree
+
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+type Tree struct {
+	Root *Node
+}
+
+func New() *Tree {
+	return &Tree{}
+}
+
+func (t *Tree) Insert(value int) *Tree {
+	if t.Root == nil {
+		t.Root = &Node{Value: value}
+		return t
+	}
+	t.Root.insert(value)
+	return t
+}
+
+func (n *Node) insert(value int) {
+	if value > n.Value {
+		if n.Right == nil {
+			n.Right = &Node{Value: value}
+		} else {
+			n.Right.insert(value)
+		}
+	} else {
+		if n.Left == nil {
+			n.Left = &Node{Value: value}
+		} else {
+			n.Left.insert(value)
+		}
+	}
+}
+
+func InOrder(n *Node, f func(*Node)) {
+	if n == nil {
+		return
+	}
+	InOrder(n.Left, f)
+	f(n)
+	InOrder(n.Right, f)
+}
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/vargasmesh/go-bt-service/internal/tree"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	bt := tree.New()
+	bt.Insert(10).Insert(5).Insert(15).Insert(3).Insert(7).Insert(13).Insert(17)
+	tree.InOrder(bt.Root, func(n *tree.Node) {
+		fmt.Println(n.Value)
+	})
 }
