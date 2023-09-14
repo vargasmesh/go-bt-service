9bff85a7968dc4bca2cfe1f07c77c8f91c27656a
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-09-14 17:46:13 -0300

    feat: flush data at max size

--- internal/server/tree_server.go
+++ internal/server/tree_server.go
@@ -2,20 +2,31 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"time"
 
 	"github.com/vargasmesh/go-bt-service/internal/tree"
 )
 
 type TreeServer struct {
-	Tree       *tree.Tree
-	insertChan chan int
+	Tree        *tree.Tree
+	insertChan  chan int
+	maxTreeSize int
+	currentSize int
 }
 
 func NewTreeServer() *TreeServer {
-	return &TreeServer{
-		Tree:       tree.New(),
-		insertChan: make(chan int),
+	t := &TreeServer{
+		Tree:        tree.New(),
+		insertChan:  make(chan int),
+		maxTreeSize: 5,
+		currentSize: 0,
 	}
+
+	_ = os.MkdirAll("./data", os.ModePerm)
+
+	return t
 }
 
 func (s *TreeServer) Insert(value int) {
@@ -26,7 +37,7 @@ func (s *TreeServer) Run(ctx context.Context) {
 	for {
 		select {
 		case value := <-s.insertChan:
-			s.Tree.Insert(value)
+			s.handleInsert(value)
 		case <-ctx.Done():
 			return
 		}
@@ -39,4 +50,22 @@ func (s *TreeServer) GetPreOrderTree() []int {
 		flatTree = append(flatTree, n.Value)
 	})
 	return flatTree
+}
+
+func (s *TreeServer) handleInsert(value int) {
+	s.Tree.Insert(value)
+	s.currentSize++
+	if s.currentSize == s.maxTreeSize {
+		f, err := os.Create(fmt.Sprintf("./data/%d.data", time.Now().Unix()))
+		if err != nil {
+			return
+		}
+		defer f.Close()
+
+		tree.InOrder(s.Tree.Root, func(n *tree.Node) {
+			f.WriteString(fmt.Sprintf("%d\n", n.Value))
+		})
+		s.Tree = tree.New()
+		s.currentSize = 0
+	}
 }
